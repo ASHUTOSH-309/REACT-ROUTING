608682c7771b1c92f1f1aa9e875eaf77b25da21c
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-15 10:36:00 +0530

    Routes wrapped in Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,38 @@
-
-
+import React, { Suspense } from 'react'
 import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
-
-import { Dashboard } from './pages/Dashboard'
-import { Landing } from './pages/Landing'
+const Landing = React.lazy(() => import('./pages/Landing'))
+const Dashboard = React.lazy(() => import('./pages/Dashboard'))
 
 function App() {
 
-
+  //Suspense API
 
   return (
     <>
-
-
-
       <BrowserRouter>
         <AppBar />
-        <Routes>
 
-          <Route path={"/dashboard"} element={<Dashboard />} />
-          <Route path={"/"} element={<Landing />} />
+        <Suspense fallback={<div>Loading....</div>}>
 
-        </Routes>
+          <Routes>
+            <Route path={"/dashboard"} element={<Dashboard />} />
+            <Route path={"/"} element={<Landing />} />
+          </Routes>
 
+
+        </Suspense>
       </BrowserRouter>
     </>
   )
 }
 
-
 function AppBar() {
-//useNavigate can only be used inside a component that is wrapped in Browser Router
+  //useNavigate can only be used inside a component that is wrapped in Browser Router
   const navigate = useNavigate()
   return <div>
     <div>
-      <button onClick={() => {navigate("/")}}>Landing</button>
-      <button onClick={() => {navigate("/dashboard")}} >Dashboard</button>
+      <button onClick={() => { navigate("/") }}>Landing</button>
+      <button onClick={() => { navigate("/dashboard") }} >Dashboard</button>
     </div>
   </div>
 }
